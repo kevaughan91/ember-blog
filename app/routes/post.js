6b521bc8d1af806d85150bc79793e8754c7313ad
Kelly Vaughan <kvaughan584@gmail.com>
2018-08-14 22:39:55 -0400

    fix ember rsvp import

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,9 +1,10 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default Route.extend({
     model(params) {
 
-        return Ember.RSVP.hash({
+        return hash({
             post_id: params.post_id,
             posts: [1,2,3,4,5,6,7,8]
           });
