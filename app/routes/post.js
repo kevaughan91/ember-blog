470ec5e23ca8293b122b5d2a261d5350ce95ad79
Kelly Vaughan <kvaughan584@gmail.com>
2018-08-14 22:36:20 -0400

    Initial structure for post page

--- app/routes/post.js
+++ app/routes/post.js
@@ -2,7 +2,11 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(params) {
-        return params.post_id;
+
+        return Ember.RSVP.hash({
+            post_id: params.post_id,
+            posts: [1,2,3,4,5,6,7,8]
+          });
     }
 });
 
